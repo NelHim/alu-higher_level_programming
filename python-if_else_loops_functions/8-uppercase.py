fbe2c72bba633c4b1ac0f7012a22635bedfa2ca9
Nelson Himbaza <nelsonhimbaza@gmail.com>
2022-10-26 03:01:27 -0700

    Add 8-uppercase.py

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,6 @@
 def uppercase(str1):
     for char in str1:
         if 123 > ord(char) > 96:
-           char = chr(ord(char) - 32)
-         print('{}'.format(char), end="")
-     print('')
+            char = chr(ord(char) - 32)
+        print('{}'.format(char), end="")
+    print('')
