9733fe609df27bd4af15c28e7280397f2903b677
Nelson Himbaza <nelsonhimbaza@gmail.com>
2022-11-08 01:46:08 -0800

    Add 1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Define a square"""
+
+
+class Square:
+    """Creates a square.
+    Private instance attributes: size
+    """
+
+    def __init__(self, size):
+        """Initializes size"""
+        self.__size = size
