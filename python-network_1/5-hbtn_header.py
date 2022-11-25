9587b96fd548eb24d3edd882db2076c544a0c3aa
Nelson Himbaza <nelsonhimbaza@gmail.com>
2022-11-25 06:59:57 -0800

    Remove buggy code

--- python-network_1/5-hbtn_header.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Documented now"""
-import requests
-import sys
-
-if __name__ == '__main__':
-    url = sys.argv[1]
-    response = requests.get(url)
-    print("{}".format(response.headers.get("X-Request-Id")))    
